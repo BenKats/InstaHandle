596e5adfe2deddcca56198db1e74179243943062
Benjamin Kats <katsbenjamin@gmail.com>
2019-05-11 20:54:10 -0400

    Succesful Login! and some linting

--- instahandle1.py
+++ instahandle1.py
@@ -1,37 +1,44 @@
 #!/usr/local/bin/python3
 import mechanize
 from bs4 import BeautifulSoup
-import requests
 import json
 
 def main():
-    url = 'https://www.instagram.com/accounts/login/?force_classic_login'
+    base_url = 'https://www.instagram.com/'
+    url = base_url + 'accounts/login/?force_classic_login'
+    logout_url = base_url + 'accounts/logout'
     username = input("Enter Username: ")
     password = input("Enter Password: ")
     post_params = {'username': username, 'password': password}
-    browser = mechanize.Browser()
-    browser.set_handle_robots(False)   
-    browser.set_handle_refresh(False)  
-    browser.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
 
-    response = browser.open(url)
+    br = mechanize.Browser()
+    br.set_handle_robots(False)   # ignore robots
+    br.set_handle_refresh(False)  # can sometimes hang without this
+    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+
+
+    response = br.open(url)
     # # print(response.read())
 
-    for f in br.forms():
-        print("Form name:",f.name)
-        print(str(f))
+    # for f in br.forms():
+    #     print("Form name:",f.name)
+    #     print(str(f))
+
     br.form = list(br.forms())[0]
-    print(br)
+    # for control in br.form.controls:
+    #     print(control)
+    #     print("type=%s, name=%s value=%s" % (control.type, control.name, br[control.name]))
 
-    # br.submit()
-    # response = requests.post(url, data=post_params)
-    # soup = BeautifulSoup(response.text, 'html.parser')
-    # print(soup)
+    br.form['username'] = username
+    br.form['password'] = password
+    br.submit()
 
-    # ses = requests.session()
-    # url2 = "https://www.instagram.com/accounts/login/ajax/"
-    # req = ses.post(url2, json.dumps(post_params))
-    # print(req.content)
+    print('##################################################')
+    print(response.read())
+    print('##################################################')
+    resp2 = br.open(base_url + username )
+    print(resp2.read())
+    br.open(logout_url)
 
 if __name__ == '__main__':
     main()
